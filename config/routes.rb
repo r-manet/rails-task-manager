97fb0f6f5ab0ae9d34c5ec9922dff9fb2a528245
Raphaelle Manet <raphaelle.manet1@gmail.com>
2021-08-17 15:49:55 +0100

    Finished challenge 2 of the day, Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index
+  get "tasks", to: "tasks#index"
+   # new
+  get "tasks/new", to: "tasks#new", as: :new_task
+  # create
+  post "tasks", to: "tasks#create"
+  # show
+  get "tasks/:id", to: "tasks#show", as: :task
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # update
+  patch "tasks/:id", to: "tasks#update"
+  # delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
